1553ce7f881184da1d609ed86f4bd891cca28ae6
Soret Brandon <brandonsoret@hotmail.fr>
2019-04-29 11:48:38 +0200

    Base
    Initialisation du projet

--- js/deezerScript.js
+++ js/deezerScript.js
@@ -1,34 +1,12 @@
-console.log('Loading Parameters');
+/* DIV DEDIER A DEEZER */
+let deezerAPI = document.getElementById('api-deezer');
 
-/* EVITER LE BLOCAGE LORS DU CHARGEMENT DE L'API DEEZER */
-window.dzAsyncInit = function () {
-    DZ.init({
-        appId: '345122',
-        channelUrl: 'http://localhost:8080/html/index.html'
-    });
-};
-(function () {
-    var e = document.createElement('script');
-    e.src = 'https://e-cdns-files.dzcdn.net/js/min/dz.js';
-    e.async = true;
-    document.getElementById('dz-root').appendChild(e);
-}());
-
-/* EXECUTION DU PLAYER */
-sdk_options = 
-{
-    token: {
-		access_token: ' b987b9429bdfdef50f81e426da373f63',
-		expire: '196966942',
-	},
-	player: {
-		current_track: {},
-		muted: false,
-		repeat: 1,
-		shuffle: false,
-		volume: 100,
-	}
-}
-DZ.ready(function(sdk_options){
-	console.log('DZ SDK is ready', sdk_options);
-});
+/*RECUPERER VIA API*/
+$.ajax({
+    url : 'https://api.deezer.com/search?q=eminem&output=jsonp',
+    dataType : 'jsonp'
+}).done(function(musiques){
+    for (const key in musiques.data) {
+            console.log(musiques.data[key]);
+    }
+})
